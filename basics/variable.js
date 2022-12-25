4afcda0c3a895316df0403d137de0bdc966133f5
Lakshman76 <107758274+Lakshman76@users.noreply.github.com>
2022-12-25 14:37:17 +0000

    done

---
+++ basics/variable.js
@@ -0,0 +1,13 @@
+const accountId=143;
+let accountEmail="abc@gmail.com";
+var accountPassword="98765";//recommended not use because of block and functional scope.
+accountCity="Ranchi";// it is also not use
+let accountState;
+// accountId="431"; // can't change value.
+
+accountEmail="xyz@google.com";
+accountPassword="45678"
+accountCity="Giridih";
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
