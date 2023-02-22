c35c35c25434118bfe0693134ed179d071d12bc0
Lakshman kumar <107758274+Lakshman76@users.noreply.github.com>
2023-02-22 16:00:01 +0000

    done

--- basics/dataType.js
+++ basics/dataType.js
@@ -1,32 +1,47 @@
-// 1. Number
+//  **************** Primitive *************** 
+// 1. Number:- It can be any ineteger value, float value and NaN(not a number) and infinity
 let num=15;
-console.log(typeof(num));
+let not_a_num = NaN;
+console.log(typeof(num));// number
+console.log(typeof(not_a_num));// number
+let infinity = Number.NEGATIVE_INFINITY;
+console.log(infinity); //  -Infinity
+console.log(typeof(infinity));// number
 
-// 2. String
+// 2. String:- It is a sequence of character enclosed in single or double quote.
 let str="Lakshman"
-console.log(typeof(str));
+console.log(typeof(str));// string
 
-// Boolean
+// Boolean:- It has a logical values that is true and false
 let is_js=true;
-console.log(typeof(is_js));
+console.log(typeof(is_js));// boolean
 
-// Undefined
+// Undefined:- only declare variable but not assign any value. 
 let un_assigned;
-console.log(typeof(un_assigned)); // if we don't assign any value and try to find type of that variable then inthis cases it gives output as undefined
+console.log(typeof(un_assigned)); // undefined
 
-// NULL
+// NULL:- It means nothing or empty value (not even zero).
 let empty=null;
-console.log(typeof(empty)); // It returns object
+console.log(typeof(empty)); // object
 
+// Symbol:-It can be used as an object property key.
+
+//  ******** Non primitive *************
+//Array:- It a special type of object that is used to store a collection of elements.Each element can be of any data type and can be accessed by index. It is zero based index.
 const heros = ["shaktiman", "naagraj", "doga"];
 console.log(typeof (heros)); // It also returns object
 
+//Object:- It is a collection of property has a name and a value.
 let myObj = {
     name: "Lakshman",
     age: 18,
+    branch: "cs"
 }
-console.log(typeof (myObj)); // it returns object
+console.log(typeof (myObj)); // object
+console.log(myObj);
+console.log(myObj.name);
 
+// Function
 const myFunction = function(){
     console.log("Hello world");
 }
---
+++ basics/typeOf.js
@@ -0,0 +1,8 @@
+let num1 = 9;
+let num2 = 26;
+let sum=num1+num2;
+console.log(typeof(sum));
+
+let str="Lakshman"
+console.log(typeof(num1+str));
+console.log(typeof(str+num1));
