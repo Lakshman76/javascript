b4616a7cbd1cbc9a1267f17f154e0c9a383179ef
Lakshman kumar <107758274+Lakshman76@users.noreply.github.com>
2023-02-22 16:42:50 +0000

    done

---
+++ basics/operators.js
@@ -0,0 +1,7 @@
+// comparison operators
+
+let num1 = 100;
+let str1 = "Lakshman"
+
+console.log(num1 == str1); // true  -> content comparison
+console.log(num === str1); // false  -> content as well as data type comparison in strict equal.
