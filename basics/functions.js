1bed04b2054eb36ddbbefba04cfac8b007ef0d70
Lakshman kumar <107758274+Lakshman76@users.noreply.github.com>
2023-02-24 18:12:44 +0000

    done

--- basics/functions.js
+++ basics/functions.js
@@ -30,3 +30,39 @@ function empty_return(name) {
 empty_return();  // blank no output
 empty_return("Lakshman"); // Lakshman
 
+// Default parameter 
+function default_para(str="Lakshman") {
+    console.log(str);
+}
+default_para(); // Lakshman
+default_para("Prakash"); // prakash
+
+//  If we take two parameters and first perameter already assigned to default value, if we call function by passing 
+//  only one argument then it is also assigned to first parameter and second parameter become undefined.
+function multiply(x=5,y) {
+    return x*y;
+}
+console.log(multiply(2));  // NaN   (2 * undefined = NaN)
+
+//  But if we assigned 2nd parameter by default value and calling function by passing one argument then it is assigned
+//   to 1st parameter and give expected result . 
+function multiply2(x, y=5) {
+    return x*y;
+}
+console.log(multiply2(6)); // 30  (6 * 5 = 30)
+
+
+// function with *******Unlimited number of parameters. ********
+// if we don't know how many parameters needed then we use "arguments"
+function sumOfAllParameters() {
+    let sum=0;
+    for(let i=0; i<arguments.length; i++){
+        sum += arguments[i];
+    }
+    return sum;
+}
+let result1 = sumOfAllParameters(1,2,3,4,5);
+console.log(result1);   // 15
+
+let result2 = sumOfAllParameters(1,2,3);
+console.log(result2);   // 6
