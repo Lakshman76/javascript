373ea1e62e1bf811f1d2536be9e313231d555da9
Lakshman kumar <107758274+Lakshman76@users.noreply.github.com>
2023-03-15 14:50:58 +0000

    added

--- basics/arrowFunction.js
+++ basics/arrowFunction.js
@@ -16,7 +16,13 @@ let addThree = (x,y,z) => {
 console.log(addThree(5,7,9)); // 21
 
 // returning object
+
 function sumAndDifference(x, y) {
     return ({ sum: x + y, difference: x - y });
 }
 console.log(sumAndDifference(13,5));  // { sum: 18, difference: 8 }
+
+// or
+const Arrow_sumAndDifference = (x,y) => ({ sum: x + y, difference: x - y });
+
+console.log(Arrow_sumAndDifference(7,5));  // { sum: 12, difference: 2 }
