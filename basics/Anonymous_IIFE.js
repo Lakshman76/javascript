a0847aa587d970110d5056de2d0d26ef58603d8e
Lakshman kumar <107758274+Lakshman76@users.noreply.github.com>
2023-03-17 07:47:35 +0000

    done

--- basics/Anonymous_IIFE.js
+++ basics/Anonymous_IIFE.js
@@ -10,7 +10,15 @@ Anonymous();
 
 (function(x){
     console.log(x*x); // 25
+    console.log(x ** 3); // 125
 })(5);
+
+// or using arrow function
+((x) => {
+    console.log(x ** 2);
+})(4);
+
 (function(){
     console.log("Hello, World!"); // Hello, World!
-})(5);
+})();
+
---
+++ basics/all_for_loop.js
@@ -0,0 +1,24 @@
+/**************** TYPES OF FOR LOOPS ***************************/
+
+// simple for loop.
+let myFrineds = ['prakash','Abhisek','Anmol','Nitin','Vishal'];
+// print my friends name using for loop.
+for (let i = 0; i < myFrineds.length; i++) {
+    console.log(myFrineds[i]);    
+}
+
+// FOR IN LOOP
+const myDetail={
+    name:'Lakshman',
+    age: 18,
+    course : 'BCA'
+}
+for (const key in myDetail) {
+    console.log(key);  // give only key value for Object like - name   age   course
+    console.log(myDetail[key]); // Lakshman     18      BCA
+}
+// Acces array named myFriens using for in loop.
+for (const key in myFrineds) {
+    console.log(key);  // it gives index of all elements.
+    console.log(myFrineds[key]);  // prakash    Abhisek    Anmol   Nitin   Vishal  (all are in new line)
+}
