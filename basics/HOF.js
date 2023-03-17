0b2769c5d573748d669728ff1ec248d5d0609cb8
Lakshman kumar <107758274+Lakshman76@users.noreply.github.com>
2023-03-17 06:55:53 +0000

    added

--- basics/HOF.js
+++ basics/HOF.js
@@ -17,9 +17,36 @@ function multiplyBy(factor) {
   
 let double = multiplyBy(2);
 let triple = multiplyBy(3);
-  
 console.log(double(5)); // 10
 console.log(triple(5)); // 15
+
+
+const higherOrder = n => {
+  const oneFun = m => {
+      const twoFun = p => {
+          return m + n + p
+      }
+      return twoFun
+  }
+  return oneFun
+}
+console.log(higherOrder(2)(3)(4));
+
+
+const myNums = [2, 3, 4, 5]
+const sumArray = arr => {
+    let total = 0
+    arr.forEach(function(element){
+        total += element
+    });
+    return total
+}
+console.log(sumArray(myNums));
   
+function Hello(){
+  console.log("Hello Lakshman ! ", Math.floor(Math.random()*16) );
+}
+setInterval(Hello, 1000);
+// setTimeout(Hello, 2000);
 
 // NOTE:- all higher-order functions are first-class functions, not all first-class functions are higher-order functions
