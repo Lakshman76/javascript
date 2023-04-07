a8dae7d37fcfb92b576b742922af29bd1b3b9c86
Lakshman kumar <107758274+Lakshman76@users.noreply.github.com>
2023-04-07 10:01:34 +0000

    this keyword inside static method added

--- basics/thisKeyword.js
+++ basics/thisKeyword.js
@@ -15,3 +15,27 @@ let obj2 = {
 }
 obj2.display();  // 10
 
+// to assign property of a class we cannot use this keyword inside satic method
+class person{
+    name;
+    age;
+    static detail(name,age,course){
+        //this.name = n;
+        //this.age = age;  // TypeError: Cannot assign to read only property
+        //this.course = course;
+        console.log(`my name is ${name} and age ${age} and course is ${course}`);
+    }
+    getDetail(n,age){
+        this.name = n;
+        this.age = age;
+    }
+    // in this static method we cannot get error but from this method we cannot access name and age of person.
+    static printDetail(){
+        console.log(`Name is : ${this.name} and age is : ${this.age}`);
+    }
+}
+person.detail("laksh",18,'BCA');  // my name is laksh and age 18 and course is BCA
+
+let p = new person("Lakshman", 19);
+p.getDetail();
+person.printDetail();
