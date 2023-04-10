e90736314f6a4daba00fe9d398db073e7006afdd
Lakshman kumar <107758274+Lakshman76@users.noreply.github.com>
2023-04-10 09:46:31 +0000

    closure in javascript

---
+++ basics/closure.js
@@ -0,0 +1,22 @@
+/********************** closures in js *******************/
+// it is a combinition of more than one function together means nesting of function.
+// closure gives us a way to access the outer function scope from inside inner function.
+
+function outer(){
+    let outerValue = "I'm outer";
+
+    function inner(){
+        let innerValue = "I'm inner";
+        console.log(outerValue);
+
+        function innerOfInner(){
+            let innerOfInnerValue = "i'm Inner of Inner";
+            console.log(outerValue +" "+ innerValue);
+        }
+        innerOfInner();
+    }
+    inner();
+}
+outer();
+//inner();  --> error
+// console.log(outerValue);  --> eeror
