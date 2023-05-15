33083c752c38646863ef734d3833acbfa1db067a
Lakshman kumar <107758274+Lakshman76@users.noreply.github.com>
2023-05-15 12:41:12 +0000

    done

--- basics/StringMethod.js
+++ basics/StringMethod.js
@@ -1,17 +1,38 @@
-//1. at() :- 
+//1. at() :- It takes number as a parameter and return a new string. you can pass +ve and -ve integer as well.
 const superHero = "Iron-man";
-console.log(superHero.at(4)); // -
-console.log(superHero.at(7)); // n 
+console.log(superHero.at(4));  // -
+console.log(superHero.at(7));  // n 
 console.log(superHero.at(13)); // undefined
 
 const myName = "Lakshman"
-console.log(myName.at(-3)); // m
+console.log(myName.at(-3));  // m
 console.log(myName.at(-10)); // undefined
 
-// 2. charAt() :-
-console.log(myName.charAt(3)); // s 
+// 2. charAt() :- It also return new string and taking parameter as integer.
+console.log(myName.charAt(3));  // s 
 console.log(myName.charAt(10)); // 
 
-console.log(myName.charAt(-3)); // 
+console.log(myName.charAt(-3));  // 
 console.log(myName.charAt(-10)); // 
-console.log(myName.charAt(5)); // m 
+console.log(myName.charAt(5));   // m 
+
+// 3. concat() :- It is used to concatinate two or more than two string.
+let str1 = "Hello";
+let str2 = "World";
+console.log(str1.concat(str2));        // HelloWorld
+console.log(str1.concat(" ",str2));    // Hello World
+console.log(str1.concat(", ",str2));   // Hello, World
+console.log(str1.concat(" Lakshman")); //Hello Lakshman
+
+// 4. endsWith() :- it specifies that whether a string ends with the characters of a specified string, returning true or false as appropriate.
+/* Syntax-
+endsWith(searchString)
+endsWith(searchString, endPosition) - endposition means (index of last character + 1)
+*/
+//e.g.-
+const name = "Lakshman";
+const name2 = "Lakshman kumar from ranchi jharkhand";
+
+console.log(name.endsWith("man"));         // true
+console.log(name2.indexOf('i'));           // 25
+console.log(name2.endsWith("ranchi", 26)); // true
