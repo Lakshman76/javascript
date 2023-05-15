957dca969a9f6276cfd5049cb5c4d415899a39e6
Lakshman kumar <107758274+Lakshman76@users.noreply.github.com>
2023-05-15 14:49:39 +0000

    typeof in js

--- basics/typeOf.js
+++ basics/typeOf.js
@@ -1,8 +1,8 @@
 let num1 = 9;
 let num2 = 26;
 let sum=num1+num2;
-console.log(typeof(sum));// number
+console.log(typeof(sum)); // number
 
 let str="Lakshman"
-console.log(typeof(num1+str));// string
-console.log(typeof(str+num1));// string
+console.log(typeof(num1+str));  // string
+console.log(typeof(str+num1)); // string
