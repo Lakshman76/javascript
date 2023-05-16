f2984ab8c176dcf6ebcd2f8de446607a53617e4e
Lakshman kumar <107758274+Lakshman76@users.noreply.github.com>
2023-05-16 13:30:36 +0000

    anonymous and IIFE

---
+++ Practice_js/Anonymous_IIFE.js
@@ -0,0 +1,28 @@
+// Anonymous function 
+
+const Anonymous = function(){
+    console.log("Inside anonymous funcion");
+};
+Anonymous();
+
+// IIFE - Immediately invoked function expression 
+// or self invoking function 
+
+(function(x){
+    console.log(x*x); // 25
+    console.log(x ** 3); // 125
+})(5);
+
+// or using arrow function
+((x) => {
+    console.log(x ** 2);  // 16
+    console.log(x ** 3);  // 64
+})(4);
+
+(function(){
+    console.log("Hello, World!"); // Hello, World!
+})();
+// or
+ (() => {
+    console.log("Hello, World!"); // Hello, World!
+ })();
