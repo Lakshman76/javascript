c62a2f001b439c169b6731d9b6d59b6c56bbdf52
Lakshman kumar <107758274+Lakshman76@users.noreply.github.com>
2023-07-28 18:09:40 +0000

    This and arrow in javascript

--- basics_03/this_and_arrow.js
+++ basics_03/this_and_arrow.js
@@ -1,17 +1,23 @@
 const user = {
     username:"Lakshman",
     course:"BCA",
-    welcomeMessage: function(){
+    welcomeMessage1: function(){
+        // console.log(`${username}, Welcome to my profile`); // ReferenceError: username is not defined
+
+        console.log(`${this.username}, Welcome to my profile`);
+    },
+    welcomeMessage2 : () => {
         // console.log(`${username}, Welcome to my profile`); // ReferenceError: username is not defined
 
         console.log(`${this.username}, Welcome to my profile`);
     }
 }
-user.welcomeMessage(); // Lakshman, Welcome to my profile
+user.welcomeMessage1(); // Lakshman, Welcome to my profile
+user.welcomeMessage2(); // undefined, Welcome to my profile
 
 user.username = "shiva";
 
-user.welcomeMessage(); // shiva, Welcome to my profile
+user.welcomeMessage1(); // shiva, Welcome to my profile
 
 console.log(this); // {}
 /**when run console.log(this);  in browser then it give output as- window object */
